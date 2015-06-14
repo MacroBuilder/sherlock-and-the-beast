e414fcf6b2dcde31dd83be4368730cd4ac9e8ad9
T.C. <thomas.n.cool@gmail.com>
2015-06-14 13:27:49 -0700

    Update sherlock-and-the-beast.py

--- sherlock-and-the-beast.py
+++ sherlock-and-the-beast.py
@@ -1,18 +1,20 @@
-"""Having trouble with dig values >= 10000."""
-
 def beast(dig):
     maximum = -1
-    for a in range(1, dig + 1):
-        if a % 3 == 0 and (dig - a) % 5 == 0:
-            if int('5' * a + '3' * (dig - a)) > maximum:
-                maximum = int('5' * a + '3' * (dig - a))
-        elif a % 5 == 0 and (dig - a) % 3 == 0:
-            if int('3' * a + '5' * (dig - a)) > maximum:
-                maximum = int('3' * a + '5' * (dig - a))
+    if dig % 3 == 0:
+        maximum = int('5' * dig)
+    else:
+        test_num = dig
+        count = 0
+        while test_num % 3:
+            test_num -= 5
+            count += 5
+        if test_num < 0:
+            return maximum
+        else:
+            maximum = int(test_num * '5' + count * '3')
     return maximum
-                
-test_cases = int(raw_input().strip())
 
-for _ in range(test_cases):
+test_cases = int(raw_input().strip())
+for _ in xrange(test_cases):
     digits = int(raw_input().strip())
     print beast(digits)
